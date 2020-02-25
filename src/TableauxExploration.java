c10a2139ce08f5f9d0c585dc090b442c65bf5dd8
Raymond-Plourde <1751228@etu.cegep-lanaudiere.qc.ca>
2020-02-25 15:32:21 -0500

    Finaliser

--- src/TableauxExploration.java
+++ src/TableauxExploration.java
@@ -5,24 +5,77 @@
 
 public class TableauxExploration {
 
-    public static void initTen(){
+    public static void initTen(int[][] tab){
+
+        int nbLignes = tab.length;
+        int nbColonnes = tab[0].length;
+
+        for(int i = 0; i < nbLignes; i++){
+            for(int j = 0; j < nbColonnes; j++){
+                tab[i][j] = 10;
+            }
+        }
     }
 
-    public static void initDiagonaleTen(){
+    public static void initDiagonaleTen(int[][] tab){
+
+        int nbLignes =tab.length;
+        int nbColonnes = 0;
+
+        for (int i = 0; i < nbLignes; i++){
+         tab[i][nbColonnes] = 10;
+         nbColonnes++;
+        }
     }
 
-    public static void initPosition(){
+    public static void initPosition(String[][] tab){
+
+        int nbLignes = tab.length;
+        int nbColonnes =tab[0].length;
+
+        for (int i = 0; i < nbLignes; i++){
+            for (int j = 0; j < nbColonnes; j++){
+                tab[i][j] = i + ":" + j;
+            }
+        }
     }
 
-    public static void isTabCarre(){
+    public static boolean isTabCarre(int[][] tab){
+
+        int nbLignes = tab.length;
+        int nbColonnes = tab[0].length;
+
+        if (nbLignes == nbColonnes){
+            return true;
+        }else{
+            return false;
+        }
     }
 
-    public static void isTabDimEgal(){
+    public static boolean isTabDimEgal(int[][] tab, int[][] tab2){
+
+        int nbLignes = tab.length;
+        int nbColonnes = tab[0].length;
+
+        int nbLignes2 = tab2.length;
+        int nbColonnes2 = tab2[0].length;
+
+        if (nbLignes == nbLignes2 && nbColonnes == nbColonnes2){
+            return true;
+        }else{
+            return false;
+        }
     }
 
-    public static void getSommeMatrices(){
+    public static int[][] getSommeMatrices(int[][] matrice1, int[][] matrice2){
+
+        int[][] sommeMatrice = new int[matrice1.length][matrice1[0].length];
+            for (int i = 0; i < sommeMatrice.length; i++)
+                for (int j = 0; j < sommeMatrice[0].length; j++){
+                    sommeMatrice[i][j] = matrice1[i][j] + matrice2[i][j];
+                }
+            return sommeMatrice;
+        }
     }
 
-
-}
 
